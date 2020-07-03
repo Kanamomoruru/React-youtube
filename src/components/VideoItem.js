00984d4c222b0530ba55ae534d1da56cc7182e37
Kanata <thunderpooh3@gmail.com>
2020-07-03 21:41:01 +0900

    create video item

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import { Grid, Paper, Typography } from '@material-ui/core';
+import { findByLabelText } from '@testing-library/react';
 
-const VideoItem = () => {
+const VideoItem = ({ video }) => {
     return(
-        <div>Video Item</div>
+        <Grid item xs={12}>
+            <Paper style={{ display: 'flex', alignItems: 'center' }}>
+                <img style={{ marginRight: '20px'}} alt="thumbnail" src={video.snippet.thumbnails.medium.url} />
+                <Typography variant='subtitle1'><b>{video.snippet.title}</b></Typography>
+            </Paper>
+        </Grid>
     )  
 }
 
